a8bfd2ea2a99702749765c73521e89349f45c3d1
Niko Pinnis <niko.pinnis@gmail.com>
2019-02-21 23:36:04 +0200

    Add comments to middleware and extend about page

--- middleware/index.js
+++ middleware/index.js
@@ -2,25 +2,23 @@ const User = require("../models/User");
 const Reservation = require("../models/Reservation");
 
 const isLoggedIn = function (req, res, next) {
-    // console.log(req.isAuthenticated());
-    if(req.isAuthenticated()) return next();
+    if (req.isAuthenticated()) return next();
     else {
         req.flash("error", "You must be logged in to see this page.");
         res.redirect('/users/login');
     }
 };
 
-const isLoggedAsAdmin = async function(req,res,next) {
-    if(req.isAuthenticated()) {
-        // console.log(req.session.passport.user);
+const isLoggedAsAdmin = async function (req, res, next) {
+    if (req.isAuthenticated()) {
         let isAdmin = false;
-        await User.findOne({username: req.session.passport.user}, "admin", (err, result)=> {
+        await User.findOne({ username: req.session.passport.user }, "admin", (err, result) => {
             // console.log(result);
-            if(err) console.error(err);
+            if (err) console.error(err);
             else isAdmin = result.admin;
         });
-        if(isAdmin) return next();
-        else  {
+        if (isAdmin) return next();
+        else {
             req.flash("error", "You must be logged in as administrator to see this page.");
             return res.redirect('/users/login');
         }
@@ -30,17 +28,18 @@ const isLoggedAsAdmin = async function(req,res,next) {
     }
 };
 
-const isOwnerOfReservation = function(req,res,next) {
-    if(req.user.admin) return next();
-    Reservation.findById(req.params.id,"user",function (err, reservation) {
-        if(err) {
+// checks if currently loggedin user ownes the reservation 
+const isOwnerOfReservation = function (req, res, next) {
+    if (req.user.admin) return next();
+    Reservation.findById(req.params.id, "user", function (err, reservation) {
+        if (err) {
             console.error(err);
             res.redirect('/');
         } else {
-            if(reservation.user==req.user.id){
+            if (reservation.user == req.user.id) {
                 return next();
             } else {
-                req.flash('error',"You are not the owner of this reservation.");
+                req.flash('error', "You are not the owner of this reservation.");
                 res.redirect('/reservations');
             }
 
@@ -48,30 +47,29 @@ const isOwnerOfReservation = function(req,res,next) {
     });
 };
 
-const isOwnerOfAccount = function(req,res,next) {
-    if(req.user && req.user.admin) return next();
-    if(req.user && req.params.id !== req.user.id) return res.redirect(`/users/${req.user.id}/show`);
-    if(req.user && req.user.id===req.params.id) {
+// checks if user is trying to modify another account
+const isOwnerOfAccount = function (req, res, next) {
+    if (req.user && req.user.admin) return next();
+    if (req.user && req.params.id !== req.user.id) return res.redirect(`/users/${req.user.id}/show`);
+    if (req.user && req.user.id === req.params.id) {
         // console.log('id match');
         return next();
-    } else{
-        res.redirect(req.user && req.user.id ? `/users/${req.user.id}/show`:"/");
-        // res.redirect(`/users/${req.user && req.user.id?req.user.id+'/show':""}`);
+    } else {
+        res.redirect(req.user && req.user.id ? `/users/${req.user.id}/show` : "/");
     }
 };
 
-const hasContactDetails = function(req,res,next){
-    User.findById(req.user.id,function (err, user) {
-        if(err){
+// this checks if the user has provided personal info before making a reservation
+const hasContactDetails = function (req, res, next) {
+    User.findById(req.user.id, function (err, user) {
+        if (err) {
             console.error(err);
             res.redirect('/');
         } else {
             console.log(user);
-            if(user.firstname && user.lastname && user.address && user.postalCode && user.phone && user.email){
-                // console.log("has contact");
+            if (user.firstname && user.lastname && user.address && user.postalCode && user.phone && user.email) {
                 next();
             } else {
-                // console.log("has NOT contact");
                 req.flash('warning', "You must provide all your contact information before making a reservation.");
                 res.redirect(`/users/${req.user.id}/edit`);
             }
